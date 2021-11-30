c9ca1dc2d83c249c28e503de71c3d2d8917e90ce
siddharth <siddharth220719@gmail.com>
2021-11-30 13:46:50 +0530

    highlighted nav on selection,changed button styles

---
+++ public/javascripts/highlight.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+  const url = document.location.href.toLowerCase();
+
+  if (url.includes('user')) {
+    const element = (document.querySelector('#users').className = 'active');
+    return;
+  }
+
+  if (url.includes('subjects')) {
+    const element = (document.querySelector('#subjects').className = 'active');
+    return;
+  }
+  if (url.includes('questions')) {
+    const element = (document.querySelector('#questions').className = 'active');
+    return;
+  }
+  if (url.includes('exam')) {
+    const element = (document.querySelector('#exams').className = 'active');
+    return;
+  }
+  if (url.includes('results')) {
+    const element = (document.querySelector('#results').className = 'active');
+    return;
+  }
+  if (url.includes('settings')) {
+    const element = (document.querySelector('#settings').className = 'active');
+    return;
+  }
+  if (url.includes('emails')) {
+    const element = (document.querySelector('#email').className = 'active');
+    return;
+  }
+  if (url.includes('reports')) {
+    const element = (document.querySelector('#reports').className = 'active');
+    return;
+  }
+  if (url.includes('feedback')) {
+    const element = (document.querySelector('#feedback').className = 'active');
+    return;
+  }
+  if (url.includes('Categories')) {
+    const element = (document.querySelector('#category').className = 'active');
+    return;
+  }
+};
